0af10e10671a46e68c49ac1db855ad18c916f17f
Demissew (Home) <dhex30@gmail.com>
2021-07-21 22:38:14 +0300

    webpack upgraded to 5 sytle loaders insalled and tested

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './style.scss';
 
 const Button = () => {
   const [count, setCount] = React.useState(0);
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,39 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {                        
-                        presets: [
-                            // Used to generate a complied js code that can run on most browsers                     
-                            '@babel/preset-env',
-                            // JSX
-                            '@babel/preset-react',
-                        ]
-                    }
-                }
-            }
-        ]
-    }
-}
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              // Used to generate a complied js code that can run on most browsers
+              '@babel/preset-env',
+              // JSX
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
